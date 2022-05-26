47c9cd8d824d5cdfda27dcdb759345d5c4236b4d
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2022-05-25 21:38:53 -0400

    add js file with mobile  menu functionality

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ const concerts = [
 
 let listBands = '';
 
-for (let i = 0; i <= concerts.length; i += 1) {
+for (let i = 0; i < concerts.length; i += 1) {
   // eslint-disable-next-line no-unused-vars
   listBands += `<div class="band-box">
   <div class="band-image">
@@ -60,136 +60,22 @@ for (let i = 0; i <= concerts.length; i += 1) {
   </div>`;
 
   document.getElementById('list-bands').innerHTML = listBands;
-  
 }
 
+/* FROM HERE CODE TO OPEN AND CLOSE MOBILE MENU FOR CAPSTONE PROJECT */
 
+const mobileMenuButtonOpen = document.querySelector('.mobile-menu-open');
+const mobileMenuButtonClose = document.querySelector('.mobile-menu-close');
+const mobileMenu = document.querySelector('nav');
 
-/* fffffffffffffffffffffffffffffff */
-
-
-
-/*
-const img = document.createElement('img');
-
-img.src = concerts[0].image;
-img.alt = 'Logo Javascript';
-
-document.body.appendChild(img);
-
-var targetDiv = document.getElementById('list-bands');
-targetDiv.innerHTML += 'data that you want to add';
-
-
-
-
-/* xxxxxxxxxxxxxcodigo de mauricio xxxxxxxxxxxxxxxxxxxxxxxxx */
-/*
-
-const body = document.querySelector('body');
-const FeaturedSection = document.querySelector('.featured-guests');
-for (let i = 0; i < featuredGuest.length; i += 1) {
-    const guestContent = document.createElement('div');
-   guestContent.classList.add('guest-content');
-    guestContent.innerHTML = `
-    <div class="guest-content--img">
-    <img src=${featuredGuest[i].image} alt="">
-</div>
-<div class="guest-content--description">
-    <h2 class="guest-name">${featuredGuest[i].name}</h2>
-    <p class="guest-content--description1">${featuredGuest[i].description}</p>
-    <div class="guest-content--border"></div>
-    <p class="guest-content--description2">${featuredGuest[i].description2}</p>
-</div>
-    `;
-    body.appendChild(guestContent);
-  };
-
-
-
-
-
-
-
-
-
-
-/*
-
-const modal = document.querySelector('#modalWin');
-
-/* FROM HERE CODE FOR MENU PROJECT */
-
-
-
-const ul = document.getElementById('show');
-const open = document.getElementById('open');
-const links = document.querySelectorAll('.menu-link');
-
-if (ul.classList.contains('show')) {
-  ul.classList.remove('show');
-}
-
-links.forEach((link) => {
-  link.addEventListener('click', () => {
-    ul.classList.remove('show');
-  });
+mobileMenuButtonOpen.addEventListener('click', () => {
+  mobileMenuButtonOpen.style.opacity = 0;
+  mobileMenu.classList.remove('nav-initial');
+  mobileMenu.classList.add('nav-active');
 });
 
-open.addEventListener('click', () => {
-  ul.classList.add('show');
+mobileMenuButtonClose.addEventListener('click', () => {
+  mobileMenu.classList.remove('nav-active');
+  mobileMenu.classList.add('nav-initial');
+  mobileMenuButtonOpen.style.opacity = 1;
 });
-
-/* FROM HERE CODE FOR POPUP PROJECT */
-
-/*
-
-const modalClose = (opc = false) => {
-  if (opc) {
-    modal.classList.remove('modal_show');
-    modal.classList.add('modal_hide');
-    setTimeout(() => {
-      modal.style.display = 'none';
-    }, 500);
-  }
-};
-
-const modalOpen = (num = null) => {
-  if (num != null) {
-    const langs = projects[num].languages;
-
-    let showLangs = '';
-
-    langs.forEach((item) => { showLangs += `<li>${item}</li>`; });
-
-    document.getElementById('modal_title').innerHTML = projects[num].client;
-    document.getElementById('modal_image').src = `${projects[num].image}`;
-    document.getElementById('modal_description').innerHTML = projects[num].description;
-    document.getElementById('modal_lang').innerHTML = showLangs;
-    document.getElementById('modal_live').href = projects[num].link;
-    document.getElementById('modal_source').href = projects[num].source;
-
-    modal.style.display = 'flex';
-
-    setTimeout(() => {
-      modal.classList.remove('modal_hide');
-      modal.classList.add('modal_show');
-    }, 100);
-  }
-};
-
-function validate() {
-  const email = document.getElementById('mail').value;
-  if (email === email.toLowerCase()) {
-    return true;
-  }
-  const message = document.getElementById('errorMessage');
-  message.innerText = 'Please, type your email in lowercase';
-  return false;
-}
-
-modalOpen();
-modalClose();
-validate();
-
-*/
